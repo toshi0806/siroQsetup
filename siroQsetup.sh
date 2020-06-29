9ab80396067ca7a1827468a6d40525b622f398c3
Toshihiko SHIMOKAWA <toshi@smkwlab.net>
2020-06-29 21:42:39 +0900

    環境変数の鯛をダブルクオートでくくって、空白が入った PATH名に対応

--- siroQsetup.sh
+++ siroQsetup.sh
@@ -16,14 +16,14 @@ sudo apt install ruby -y
 git clone https://github.com/tmori/athrill.git
 git clone https://github.com/tmori/athrill-target.git
 (cd athrill-target/v850e2m/build_linux; make timer32=true clean; make timer32=true)
-export PATH=${HOME}/athrill/bin/linux:${PATH}
+export PATH="${HOME}/athrill/bin/linux:${PATH}"
 
 # install 64bit gcc
 wget https://github.com/toppers/athrill-gcc-v850e2m/releases/download/v1.1/athrill-gcc-package.tar.gz
 tar xzf athrill-gcc-package.tar.gz
 (cd athrill-gcc-package; tar xzvf athrill-gcc.tar.gz; sudo mv usr/local/athrill-gcc /usr/local)
-export PATH=/usr/local/athrill-gcc/bin/:${HOME}/athrill/bin/linux:${PATH}
-export LD_LIBRARY_PATH=/usr/local/athrill-gcc:/usr/local/athrill-gcc/lib:${LD_LIBRARY_PATH}
+export PATH="/usr/local/athrill-gcc/bin/:${HOME}/athrill/bin/linux:${PATH}"
+export LD_LIBRARY_PATH="/usr/local/athrill-gcc:/usr/local/athrill-gcc/lib:${LD_LIBRARY_PATH}"
 
 # install EV3RT development environment
 git clone https://github.com/tmori/athrill-sample.git
@@ -31,6 +31,6 @@ git clone https://github.com/tmori/athrill-sample.git
 
 # setup bash initialize file
 cat <<EOF >>${HOME}/.bashrc
-export PATH=/usr/local/athrill-gcc/bin/:\${HOME}/athrill/bin/linux:\${PATH}
-export LD_LIBRARY_PATH=/usr/local/athrill-gcc:/usr/local/athrill-gcc/lib:\${LD_LIBRARY_PATH}
+export PATH=\"/usr/local/athrill-gcc/bin/:\${HOME}/athrill/bin/linux:\${PATH}\"
+export LD_LIBRARY_PATH=\"/usr/local/athrill-gcc:/usr/local/athrill-gcc/lib:\${LD_LIBRARY_PATH}\"
 EOF
